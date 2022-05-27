0a667b08276992bba82470aea5f7d15b4e313fde
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-05-27 21:55:44 +0200

    init store redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,9 @@
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import articleReducer from "./articles/articleReducer";
+import thunk from "redux-thunk";
 
+const rootReducer = combineReducers(articleReducer);
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
