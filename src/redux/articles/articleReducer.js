12517e0dbd05d3888ff1991e2b0af450a312588e
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-05-27 22:17:57 +0200

    articles fetch api

--- src/redux/articles/articleReducer.js
+++ src/redux/articles/articleReducer.js
@@ -15,4 +15,13 @@ function articleReducer(state = INITIAL_STATE, action) {
 
 export default articleReducer;
 
-
+export const getArticles = () => (dispatch) => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+  .then(response => response.json())
+  .then(data => {
+      dispatch({
+          type: "LOADARTICLES", 
+          payload: data
+      })
+  })
+};
