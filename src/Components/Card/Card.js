3d467e3c9886531096945440544cff5691fdb647
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-05-28 00:37:33 +0200

    loop data in Home

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,10 +1,8 @@
 import "./Card.css";
 
-import { v4 as uuidv4 } from "uuid";
-
 function Card({ title }) {
     return (
-      <div className="card" key={uuidv4()}>
+      <div className="card">
         <h2>{title}</h2>
         <a href="/">Lire l'article</a>
       </div>
--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -3,6 +3,8 @@ import "./Home.css";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import { getArticles } from "../../redux/articles/articleReducer";
+import { v4 as uuidv4 } from "uuid";
+
 
 function Home() {
   const { articles } = useSelector((state) => ({
@@ -22,7 +24,7 @@ function Home() {
       <h1 className="home-title">Les articles</h1>
       <div className="container-cards">
         {articles.map((article) => {
-          return <Card title={article.title} />;
+          return <Card title={article.title} key={uuidv4()} />;
         })}
       </div>
     </>
