1e7a59e3744a5075f6f035e898a55880658ed63c
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-05-30 23:16:32 +0200

    Test form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,15 +1,31 @@
-import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import "./Form.css";
 
 function Form() {
+  const [article, setArticle] = useState({
+    title: "",
+    body: "",
+  });
+
+  const dispatch = useDispatch();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+  };
+
+  const handleInputs = (e) => {
+    console.log("test change");
+  };
+
   return (
     <>
       <h1 className="title-form">Écrivez un article </h1>
-      <form className="container-form">
+      <form onSubmit={handleForm} className="container-form">
         <label htmlFor="title">Titre</label>
-        <input id="title" type="text" />
+        <input onChange={handleInputs} id="title" type="text" />
         <label htmlFor="article">Votre article</label>
-        <textarea id="article" placeholder="Votre article"></textarea>
+        <textarea onChange={handleInputs} id="article" placeholder="Votre article"></textarea>
         <button>Publier l'article</button>
       </form>
     </>
