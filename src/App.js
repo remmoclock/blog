98ea3e1160e4453c1e20065edd93984279348d30
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-05-31 12:19:48 +0200

    create unique route for each article

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Containers/Home/Home";
 import AddArticle from "./Containers/AddArticle/AddArticle";
 import Contact from "./Containers/Contact/Contact";
+import Article from "./Containers/Article/Article"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/publier" element={<AddArticle />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/articles/:id" element={<Article />} />
       </Routes>
     </div>
   );
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,11 +1,19 @@
 import "./Card.css";
+import { Link } from "react-router-dom";
 
 function Card({ title }) {
-    return (
-      <div className="card">
-        <h2>{title}</h2>
-        <a href="/">Lire l'article</a>
-      </div>
-    );
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+      <Link
+        to={{
+          pathname: `articles/${title}`,
+          state: { fromDashboard: true },
+        }}
+      >
+        Lire l'article
+      </Link>
+    </div>
+  );
 }
 export default Card;
